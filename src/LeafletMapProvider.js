36d698a4b33abeaf8046e2bf961b35e97661624f
Marc Saleiko <marc.saleiko@komadesign.de>
2020-09-07 10:16:33 +0200

    [FEATURE] Adds first iteration of GoogleMaps support

--- src/LeafletMapProvider.js
+++ src/LeafletMapProvider.js
@@ -1,89 +1,94 @@
 window.LeafletMapProvider = (function () {
-    var _init = false;
-    var app = {};
-    var map = false;
-    var tileLayer = false;
-    var mapMarker = [];
-    var settings = {
-      // accepts "normal" or "wms"
-      tileLayerType: 'normal',
-      tileLayerUrl: 'https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png',
-      tileLayerOptions: {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      },
-      markerPopupAnchor: [0, -40],
-    };
-  
-    app.init = function( options ) {
-      if( _init ) {return; }
-      _init = true;
-      settings = Object.assign(settings, options);
-    };
-  
-    app.initMap = function( mapElement, mapSettings ) {
-      map = L.map(mapElement).setView(mapSettings.mapDefaultCenter, mapSettings.mapDefaultZoom);
-  
-      if( settings.tileLayerType === 'normal' ) {
-        tileLayer = L.tileLayer(settings.tileLayerUrl, settings.tileLayerOptions ).addTo(map);
+  var _init = false;
+  var app = {};
+  var map = false;
+  var tileLayer = false;
+  var mapMarker = [];
+  var hasRemoteLibraryToLoad = false;
+  var settings = {
+    // accepts "normal" or "wms"
+    tileLayerType: 'normal',
+    tileLayerUrl: 'https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png',
+    tileLayerOptions: {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    },
+    markerPopupAnchor: [0, -40],
+  };
+
+  app.init = function( options ) {
+    if( _init ) {return; }
+    _init = true;
+    settings = Object.assign(settings, options);
+  };
+
+  app.hasRemoteLibraryToLoad = function(){
+    return hasRemoteLibraryToLoad;
+  };
+
+  app.initMap = function( mapElement, mapSettings ) {
+    map = L.map(mapElement).setView(mapSettings.mapDefaultCenter, mapSettings.mapDefaultZoom);
+
+    if( settings.tileLayerType === 'normal' ) {
+      tileLayer = L.tileLayer(settings.tileLayerUrl, settings.tileLayerOptions ).addTo(map);
+    }
+    else if (settings.tileLayerType === 'wms' ) {
+      tileLayer = L.tileLayer.wms(settings.tileLayerUrl, settings.tileLayerOptions).addTo(map);
+    }
+    else {
+      console.error('LeafletMapProvider: tileLayerType '+settings.tileLayerType+' does not have an implementation.');
+    }
+
+  };
+
+  app.setMarker = function( marker, mapSettings ) {
+
+    var defaultIcon = L.icon({
+      iconUrl: mapSettings.markerImagePath,
+      iconSize: mapSettings.markerIconSize,
+      iconAnchor: mapSettings.markerIconAnchor,
+      popupAnchor: settings.markerPopupAnchor,
+    });
+
+    var latLngBounds = [];
+    for( var i = 0; i < marker.length; i++ ) {
+
+      mapMarker[i] = L.marker( [marker[i].latitude, marker[i].longitude], {
+          icon: defaultIcon,
+      }).addTo(map);
+
+      latLngBounds.push([marker[i].latitude, marker[i].longitude]);
+      mapMarker[i].markerData = marker[i];
+
+      if( mapSettings.showInfoWindow && typeof marker[i].infowindow !== 'undefined' && marker[i].infowindow !== '' ) {
+        mapMarker[i].bindPopup(marker[i].infowindow);
       }
-      else if (settings.tileLayerType === 'wms' ) {
-        tileLayer = L.tileLayer.wms(settings.tileLayerUrl, settings.tileLayerOptions).addTo(map);
-      }
-      else {
-        console.error('LeafletMapProvider: tileLayerType '+settings.tileLayerType+' does not have an implementation.');
-      }
-  
-    };
-  
-    app.setMarker = function( marker, mapSettings ) {
-  
-      var defaultIcon = L.icon({
-        iconUrl: mapSettings.markerImagePath,
-        iconSize: mapSettings.markerIconSize,
-        iconAnchor: mapSettings.markerIconAnchor,
-        popupAnchor: settings.markerPopupAnchor,
-      });
-  
-      var latLngBounds = [];
-      for( var i = 0; i < marker.length; i++ ) {
-  
-        mapMarker[i] = L.marker( [marker[i].latitude, marker[i].longitude], {
-            icon: defaultIcon,
-        }).addTo(map);
-  
-        latLngBounds.push([marker[i].latitude, marker[i].longitude]);
-        mapMarker[i].markerData = marker[i];
-  
-        if( mapSettings.showInfoWindow && typeof marker[i].infowindow !== 'undefined' && marker[i].infowindow !== '' ) {
-          mapMarker[i].bindPopup(marker[i].infowindow);
-        }
-        if( mapSettings.markerHasOnClick || mapSettings.showInfoWindow ) {
-          mapMarker[i].on('click', function() {
-            if( mapSettings.markerHasOnClick ) {
-              if( typeof mapSettings.markerOnClickCallback === 'function' ) {
-                mapSettings.markerOnClickCallback( this );
-              }
+      if( mapSettings.markerHasOnClick || mapSettings.showInfoWindow ) {
+        mapMarker[i].on('click', function() {
+          if( mapSettings.markerHasOnClick ) {
+            if( typeof mapSettings.markerOnClickCallback === 'function' ) {
+              mapSettings.markerOnClickCallback( this );
             }
-            if( mapSettings.showInfoWindow ) {
-              var infoWindowBody = '';
-              if( typeof this.markerData.infowindow !== 'undefined' && this.markerData.infowindow !== '' ) {
-                infoWindowBody = this.markerData.infowindow;
-              }
-              if( typeof mapSettings.markerInfoWindowBodyFilter === 'function' ) {
-                infoWindowBody = mapSettings.markerInfoWindowBodyFilter( this, infoWindowBody );
-              }
-              if( infoWindowBody !== '' ) {
-                this.bindPopup(infoWindowBody);
-                this.openPopup();
-              }
+          }
+          if( mapSettings.showInfoWindow ) {
+            var infoWindowBody = '';
+            if( typeof this.markerData.infowindow !== 'undefined' && this.markerData.infowindow !== '' ) {
+              infoWindowBody = this.markerData.infowindow;
             }
-          });
-        }
+            if( typeof mapSettings.markerInfoWindowBodyFilter === 'function' ) {
+              infoWindowBody = mapSettings.markerInfoWindowBodyFilter( this, infoWindowBody );
+            }
+            if( infoWindowBody !== '' ) {
+              this.bindPopup(infoWindowBody);
+              this.openPopup();
+            }
+          }
+        });
       }
-      if( mapSettings.markerFitBounds === true ) {
-        map.fitBounds(L.latLngBounds(latLngBounds));
-      }
-    };
-  
-    return app;
-  })();
+    }
+    if( mapSettings.markerFitBounds === true ) {
+      map.fitBounds(L.latLngBounds(latLngBounds));
+    }
+  };
+
+  return app;
+})();
--- src/Maps.js
+++ src/Maps.js
@@ -1,92 +1,102 @@
 window.Maps = ( function(){
-    var _init = false;
-    var app = {};
-    app.marker = [];
-    var mapContainer = false;
-    var mapVisible = false;
-    var settings = {
-      mapContainerSelector: '.js-map',
-      mapDefaultZoom: 13,
-      mapDefaultCenter: [60.394517, 5.3429066],
-      markerWindowVariableName: 'MapsMarkerData',
-      markerImagePath: '/typo3conf/ext/t3xp_sitepackage/Resources/Public/1_General/Assets/Images/marker-default.png',
-      markerIconSize: [32, 32],
-      markerIconAnchor: [16, 32],
-      markerFitBounds: true,
-      provider: false,
-      providerConfiguration: {},
-      lazyLoadMap: false,
-      showInfoWindow: false,
-      markerHasOnClick: false,
-      hideMapIfNoMarkerAvailable: false,
-      hideMapClass: 'd-none',
-      markerInfoWindowBodyFilter: false,
-      markerOnClickCallback: false,
-    };
-  
-    app.init = function( options ) {
-      if(_init){return;}
-      _init = true;
-      // extend settings by options
-      settings = Object.assign(settings, options);
-  
-      mapContainer = $$( settings.mapContainerSelector );
-      if( mapContainer.length > 0 ) {
-  
-        if( typeof window[settings.markerWindowVariableName] === 'undefined' ) {
-          console.error('Maps: window.'+settings.markerWindowVariableName+' does not exist. No marker data found. Exiting.')
-          return;
-        }
-  
-        if( settings.provider === false || typeof settings.provider === 'undefined' ) {
-          console.error('Maps: You must define a valid maps provider via settings.provider. Exiting.');
-          return;
-        }
-  
-        app.marker = window[settings.markerWindowVariableName];
-        if( app.marker.length > 0 ) {
-          settings.provider.init( settings.providerConfiguration );
-          if( settings.lazyLoadMap === false ) {
-            settings.provider.initMap( mapContainer[0], settings );
-            settings.provider.setMarker( app.marker, settings );
-            mapVisible = true;
+  var _init = false;
+  var app = {};
+  app.marker = [];
+  var mapContainer = false;
+  var mapVisible = false;
+  var settings = {
+    mapContainerSelector: '.js-map',
+    mapDefaultZoom: 13,
+    mapDefaultCenter: [60.394517, 5.3429066],
+    markerWindowVariableName: 'MapsMarkerData',
+    markerImagePath: '/typo3conf/ext/t3xp_sitepackage/Resources/Public/1_General/Assets/Images/marker-default.png',
+    markerIconSize: [32, 32],
+    markerIconAnchor: [16, 32],
+    markerFitBounds: true,
+    provider: false,
+    providerConfiguration: {},
+    lazyLoadMap: false,
+    showInfoWindow: false,
+    markerHasOnClick: false,
+    hideMapIfNoMarkerAvailable: false,
+    hideMapClass: 'd-none',
+    markerInfoWindowBodyFilter: false,
+    markerOnClickCallback: false,
+  };
+
+  app.init = function( options ) {
+    if(_init){return;}
+    _init = true;
+    // extend settings by options
+    settings = Object.assign(settings, options);
+
+    mapContainer = $$( settings.mapContainerSelector );
+    if( mapContainer.length > 0 ) {
+
+      if( typeof window[settings.markerWindowVariableName] === 'undefined' ) {
+        console.error('Maps: window.'+settings.markerWindowVariableName+' does not exist. No marker data found. Exiting.')
+        return;
+      }
+
+      if( settings.provider === false || typeof settings.provider === 'undefined' ) {
+        console.error('Maps: You must define a valid maps provider via settings.provider. Exiting.');
+        return;
+      }
+
+      app.marker = window[settings.markerWindowVariableName];
+      if( app.marker.length > 0 ) {
+        settings.provider.init( settings.providerConfiguration );
+        if( settings.lazyLoadMap === false ) {
+          // automatically trigger further initialization
+          // if we have no external libraries to load
+          // otherwise wait for provider to call
+          // remoteLibrariesHaveLoaded()
+          if( !settings.provider.hasRemoteLibraryToLoad() ) {
+            app.remoteLibrariesHaveLoaded();
           }
         }
-        else if( settings.hideMapIfNoMarkerAvailable ){
-          addClass( mapContainer, settings.hideMapClass );
-        }
       }
-    };
-  
-    app.isMapVisible = function() {
-      return mapVisible;
+      else if( settings.hideMapIfNoMarkerAvailable ){
+        addClass( mapContainer, settings.hideMapClass );
+      }
     }
-  
-    var $$ = function( selector ) {
-      return document.querySelectorAll( selector );
-    };
-  
-    var addClass = function( elements, className ) {
-      if( elements instanceof Node ) {
-        elements.classList.add( className );
+  };
+
+  app.remoteLibrariesHaveLoaded = function(){
+    settings.provider.initMap( mapContainer[0], settings );
+    settings.provider.setMarker( app.marker, settings );
+    mapVisible = true;
+  };
+
+  app.isMapVisible = function() {
+    return mapVisible;
+  }
+
+  var $$ = function( selector ) {
+    return document.querySelectorAll( selector );
+  };
+
+  var addClass = function( elements, className ) {
+    if( elements instanceof Node ) {
+      elements.classList.add( className );
+    }
+    else {
+      for( var i = 0, len = elements.length; i < len; i++) {
+        elements[i].classList.add( className );
       }
-      else {
-        for( var i = 0, len = elements.length; i < len; i++) {
-          elements[i].classList.add( className );
-        }
+    }
+  };
+
+  var removeClass = function( elements, className ) {
+    if( elements instanceof Node ) {
+      elements.classList.remove( className );
+    }
+    else {
+      for( var i = 0, len = elements.length; i < len; i++) {
+        elements[i].classList.remove( className );
       }
-    };
-  
-    var removeClass = function( elements, className ) {
-      if( elements instanceof Node ) {
-        elements.classList.remove( className );
-      }
-      else {
-        for( var i = 0, len = elements.length; i < len; i++) {
-          elements[i].classList.remove( className );
-        }
-      }
-    };
-  
-    return app;
-  })();
+    }
+  };
+
+  return app;
+})();
