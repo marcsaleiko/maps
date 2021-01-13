3d9f9ff952badea4b04d895add4f600be6de020a
Marc Saleiko <marc.saleiko@komadesign.de>
2021-01-13 11:00:06 +0100

    [FEATURE] Adds scrollWheelZoom option to leaflet provider

--- src/LeafletMapProvider.js
+++ src/LeafletMapProvider.js
@@ -14,6 +14,7 @@ window.LeafletMapProvider = (function () {
     },
     markerPopupAnchor: [0, -40],
     zoomControl: true,
+    scrollWheelZoom: true,
   };
 
   app.init = function( options ) {
@@ -28,7 +29,8 @@ window.LeafletMapProvider = (function () {
 
   app.initMap = function( mapElement, mapSettings ) {
     map = L.map(mapElement, {
-      zoomControl: settings.zoomControl
+      zoomControl: settings.zoomControl,
+      scrollWheelZoom: settings.scrollWheelZoom,
     }).setView(mapSettings.mapDefaultCenter, mapSettings.mapDefaultZoom);
 
     if( settings.tileLayerType === 'normal' ) {
